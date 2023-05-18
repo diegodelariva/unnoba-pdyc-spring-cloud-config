ca2d703fcbd59d5f951d3af8ad21d8dbe3ff68c0
Diego de la Riva <diego.delariva@edit.com.ar>
2023-05-18 18:48:01 -0300

    primer commit

---
+++ config-server/src/test/java/ar/edu/unnoba/pdyc/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unnoba.pdyc.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
